fe5378e0f58732159553e0b463f066610055dc01
asemle <asemle@umass.edu>
2016-11-17 11:56:58 -0700

    finished

--- service.js
+++ service.js
@@ -1,10 +1,19 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
+
 
   this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
+
+      var defer = $q.defer();
+
+      $http.get('http://reqres.in/api/users?page=1')
+        .then(function(response) {
+          var parsedResponse = response.data.data
+          for(var i = 0; i < parsedResponse.length; i++) {
+            parsedResponse[i].first_name = 'Ralf';
+          }
+          defer.resolve(parsedResponse)
+        })
+        return defer.promise;
   }
 
 });
